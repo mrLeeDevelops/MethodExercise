649b49220e46ef41d84e4d5333e07c7b9d90631a
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-08 11:55:26 -0500

    Add project files.

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
